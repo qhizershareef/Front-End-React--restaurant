27899a5e53dd5515d0201a9fe761686ebc81945c
qhizershareef <qhizershareef@gmal.com>
2020-04-20 15:50:08 +0530

    React onClick

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 import {Navbar,NavbarBrand} from 'reactstrap';
-
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="danger" id="navbar">
-          <div className="container">
-            <NavbarBrand href="/" id="nav">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-    </div>
-  );
+import Menu from './components/MenuList';
+import {DISHES} from './shared/dishes';
+//we can either use App() as function or 
+// As class App which extends Component and render it in index.js, when using class we do have to specify the state
+class App extends Component{
+  render(){
+    return (
+      <div >
+        <Navbar dark color="danger" id="navbar">
+            <div className="container">
+              <NavbarBrand href="/" id="nav">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu dishes={DISHES}/>
+      </div>
+    );
+  } 
 }
-
+/*
+function App() {
+  
+}
+*/
 export default App;
 /*Inside div className="App"
 <header className="App-header">
